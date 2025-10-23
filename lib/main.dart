4f73d1c27d1e5389daa20bc65c7d30af985aaee9
RohanK221 <rohan-kellaway@hotmail.com>
2025-10-24 00:01:11 +0100

    Define UI inside app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {runApp(const App());}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sandwich Shop App',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Sandwich Counter')),
+        body: const Center(
+          child: Text('Welcome to the Sandwich Shop!'),
+        ),
+      ),
+    );
+  }
+}
 //   runApp(const MyApp());
 // }
 
